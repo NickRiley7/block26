73dceaac23ae42584ad8557f942771ce8595d4fa
NickRiley7 <nick_riley@LAPTOP-J8V35VGC>
2023-11-07 16:58:20 -0500

    workshop answers

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css";
+import { useState } from "react";
+import ContactList from "./components/ContactList.jsx";
+import SelectedContact from "./components/SelectedContact.jsx";
+
+export default function App() {
+  const [selectedContactId, setSelectedContactId] = useState(null);
+
+  return (
+    <>
+      {selectedContactId ? (
+        <SelectedContact
+          selectedContactId={selectedContactId}
+          setSelectedContactId={setSelectedContactId}
+        />
+      ) : (
+        <ContactList setSelectedContactId={setSelectedContactId} />
+      )}
+    </>
+  );
+}
